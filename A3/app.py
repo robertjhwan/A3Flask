78b98641c015ec74b499b9cd6ee81dfefeb1f7fa
JasonPingChengChien <48368450+JasonPingChengChien@users.noreply.github.com>
2019-04-05 21:40:38 -0400

    Update app.py

--- A3/app.py
+++ A3/app.py
@@ -9,50 +9,48 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
+	# if the user is already logged in, just take them directly to home page. If not, redirect them to the login page.
 	if 'utorid' in session:
-		sql1 = """
-					SELECT *
-					FROM marks
-					where utorid='{}'""".format(session['utorid'])
-		results = db.engine.execute(text(sql1))
-		return render_template('index.html',results=results)
+		return render_template('index.html')
 	else:
 		return redirect(url_for('login'))
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-	if request.method=='POST':
+	# first check if the user is in session. If they are, they will be automatically redirected instead of having to log in twice.
+	if 'utorid' in session:
+			return render_template('index.html')
+	# first retrieve the data that includes all users and their passwords.
+	elif request.method=='POST':
 		sql = """
 			SELECT *
 			FROM users
 			"""
 		results = db.engine.execute(text(sql))
 		for result in results:
+			# make sure that the username and password actually matches up. If it does, redirect them to the home page
 			if result['utorid']==request.form['utorid']:
 				if result['password']==request.form['password']:
 					session['utorid']=request.form['utorid']
-					sql1 = """
-						SELECT *
-						FROM marks
-						where utorid='{}'""".format(request.form['utorid'])
-					results = db.engine.execute(text(sql1))
-					return render_template('marks.html',results=results)
-		return "Incorrect UserName/Password"
-	elif 'utorid' in session:
-			sql1 = """
-					SELECT *
-					FROM marks
-					where utorid='{}'""".format(session['utorid'])
-			results = db.engine.execute(text(sql1))
-			return render_template('marks.html',results=results)
+					return render_template('index.html')
+				else:
+					# wrong username or password
+					return render_template('login.html', wrong = "You have entered the wrong username or password")
+			# wrong username or password
+			else:
+				return render_template('login.html', wrong = "You have entered the wrong username or password")
+	# if the user clicked submit without actually entering anything at all
 	elif request.method == 'GET':
-		return render_template('login.html')
+		return render_template('login.html', wrong = "")
+	# for any other cases we haven't checked, it will just reload an empty login page
 	else:
-		return render_template('login.html')
+		return render_template('login.html', wrong = "")
 
 @app.route('/signup',methods=['GET','POST'])
 def sign_up():
 	if request.method=='POST':
+		# a very simple query to add values into the database. After it is done, it will redirect to the login page,
+		# like how all the other webpage does it.
 		utorid = request.form['utorid']
 		password = request.form['password']
 		sql1 = """
@@ -67,33 +65,41 @@ def sign_up():
 		results = db.engine.execute(text(sql1))
 
 		return redirect(url_for('login'))
-
 	elif request.method=='GET':
 		return render_template('signup.html')
 
 @app.route('/marks', methods=['GET', 'POST'])
 def marks():
 	if 'utorid' in session:
+		# select the type of user in order for us to know what to query for the user
 		sql1 = """
 			SELECT type
 			FROM users
 			where utorid='{}'""".format(session['utorid'])
 		results = db.engine.execute(text(sql1))
-		
+		# if student, retrieve all the data for that user alone
 		if results == "s":
 			sql1 = """
 				SELECT *
 				FROM marks
 				where utorid='{}'""".format(session['utorid'])	
 			results = db.engine.execute(text(sql1))
+			
 			return render_template('marksStudent.html',results=results)
-
+		# if instructor, retrieve all the data for all users
 		elif results == "i":
 			sql1 = """
 				SELECT *
-				FROM marks""".format()	
+				FROM marks""".format()
 			results = db.engine.execute(text(sql1))
-			return render_template('marksInstructor.html',results=results)
+			# it should also display the database for all the remark requests that are available
+			sql1 = """
+			SELECT *
+			FROM remarks
+			"""
+			results2 = db.engine.execute(text(sql1))
+
+			return render_template('marksInstructor.html',results=results, results2=results2)
 	else:
 		return redirect(url_for('login'))
 
@@ -101,6 +107,7 @@ def marks():
 def changeMarks():
 	if 'utorid' in session:
 		if request.method == 'POST':
+			# a query to update the mark of a particular student
 			assign = request.form["assign"]
 			mark = request.form["mark"]
 			utorid = request.form["utorid"]
@@ -110,13 +117,65 @@ def changeMarks():
 				SET '{}' = '{}'
 				where utorid='{}'""".format(assign, mark, utorid)
 			results = db.engine.execute(text(sql1))
-
-			return render_template('marksInstructor.html')
+			return redirect(url_for('marks'))
 		elif request.method == 'GET':
-			render_template('marksInstructor.html')
+			return redirect(url_for('changeMarks'))
 	else:
 		return redirect(url_for('login'))
 
+
+@app.route('/remark',methods=['GET','POST'])
+def remark():
+    if 'utorid' in session:
+        if request.method == 'POST':
+            utorid = request.form['utorid']
+            evaluation = request.form['evaluation']
+            explanation = request.form['explanation']
+            sql1 = """
+                    INSERT INTO remarks
+                    VALUES ('{}', '{}', '{}')
+                    """.format(utorid, evaluation, explanation)
+            results = db.engine.execute(text(sql1))
+            return redirect(url_for('marks'))
+        elif request.method=='GET':
+            return redirect(url_for('marks'))
+    else:
+        return redirect (url_for ('login'))
+
+@app.route('/feedback')
+def feedback():
+	if 'utorid' in session:
+		# select the type of user in order for us to know what to query for the user
+		sql1 = """
+			SELECT type
+			FROM users
+			where utorid='{}'""".format(session['utorid'])
+		results = db.engine.execute(text(sql1))
+		# if the user is a student, they can submit a anon feedback form
+		if results == "s":
+			if request.method == 'POST':
+				utorid = request.form['utorid']
+				q1 = request.form['q1']
+				q2 = request.form['q2']
+				sql1 = """
+						INSERT INTO feedback
+						VALUES ('{}', '{}', '{}')
+						""".format(utorid, q1, q2)
+				results = db.engine.execute(text(sql1))
+				return redirect(url_for('index'))
+			# if nothing on the form is entered (yet), it will load the form again
+			else:
+				return render_template('feedbackStudent.html')
+		# if it is an instructor, then the database just need to be shown accordingly
+		elif results == "i":
+			sql1 = """
+					SELECT *
+					FROM feedback
+					where utorid='{}'""".format(session['utorid'])
+			results = db.engine.execute(text(sql1))
+			return render_template('feedbackInstructor.html', results = results)
+	else:
+		return redirect(url_for('login'))
 
 @app.route('/assignment')
 def assignment():
@@ -125,6 +184,47 @@ def assignment():
 	else:
 		return redirect(url_for('login'))
 
+@app.route('/announcements')
+def annoucements():
+	if 'utorid' in session:
+		render_template('annoucements.html')
+	else:
+		return redirect(url_for('login'))
+
+@app.route('/calender')
+def calender():
+	if 'utorid' in session:
+		render_template('calender.html')
+	else:
+		return redirect(url_for('login'))
+
+@app.route('/slides')
+def slides():
+	if 'utorid' in session:
+		render_template('slides.html')
+	else:
+		return redirect(url_for('login'))
+
+@app.route('/tests')
+def tests():
+	if 'utorid' in session:
+		render_template('tests.html')
+	else:
+		return redirect(url_for('login'))
+
+@app.route('/resources')
+def resources():
+	if 'utorid' in session:
+		render_template('resources.html')
+	else:
+		return redirect(url_for('login'))
+
+@app.route('/courseteam')
+def courseteam():
+	if 'utorid' in session:
+		render_template('courseteam.html')
+	else:
+		return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
@@ -132,5 +232,4 @@ def logout():
 	return redirect(url_for('login'))
 
 if __name__=="__main__":
-	app.run(debug=True,host='0.0.0.0')
-
+	app.run(debug = True,host='0.0.0.0')
